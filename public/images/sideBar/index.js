096278e6abd6a61b28b3d16acc2d34325827334e
Anastasiia <120492521+matoka02@users.noreply.github.com>
2025-05-28 14:06:28 +0300

    add images

---
+++ public/images/sideBar/index.js
@@ -0,0 +1,12 @@
+const IMAGES = {
+    logoGooseMob: require('./logo-goose-mob.webp'),
+    logoGooseMob2x: require('./logo-goose-mob@2x.webp'),
+    logoGooseTablet: require('./logo-goose-tablet.webp'),
+    logoGooseTablet2x: require('./logo-goose-tablet@2x.webp'),
+    logoGooseDesktop: require('./logo-goose-desktop.webp'),
+    logoGooseDesktop2x: require('./logo-goose-desktop@2x.webp'),
+    headerGooseDesktop: require('./header-goose-desktop.webp'),
+    headerGooseDesktop2x: require('./header-goose-desktop@2x.webp'),
+};
+
+export default IMAGES;
